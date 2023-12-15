029e7a5bcfd140fcf923cd6c9ab2871569ab4753
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2023-12-15 12:49:02 +0000

    aDDED PRODUCT CATEGORY CONTROLLER

---
+++ src/controllers/productCategory.controller.js
@@ -0,0 +1,10 @@
+const catchAsync = require('../utils/catchAsync');
+
+// Create product category
+exports.postProductCategory = catchAsync(async (req, res, next) => {});
+
+// Get product category
+
+// Update product category
+
+// delete product category
